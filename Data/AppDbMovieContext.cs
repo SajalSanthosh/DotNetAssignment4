71d85d47366da3d8e0c2e8d00b11b7b434d3495f
SajalSanthosh <61447187+sajalsanthosh@users.noreply.github.com>
2020-07-29 23:19:40 -0400

    Init

---
+++ Data/AppDbMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment4.Models;
+
+namespace Assignment4.Data
+{
+    public class AppDbMovieContext : DbContext
+    {
+        public AppDbMovieContext (DbContextOptions<AppDbMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment4.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment4.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string MovieName { get; set; }
+        public string MovieDescription { get; set; }
+        public int Review { get; set; }
+    }
+}
